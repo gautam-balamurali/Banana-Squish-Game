1e4d3e1e2bc02977e5eead0d8fbb3db3d15e7cb2
c0d1n6 fr34k <gtmbalamurali@gmail.com>
2017-07-04 16:27:33 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+# Configuration file for Squish
+# -----------------------------
+
+# If the game is too fast or too slow, try to modify the speed
+# variables.
+# Change these to use other images in the game:
+banana_image ='banana2.png'
+weight_image ='weight2.jpeg'
+splash_image ='weight2.jpeg'
+# Change these to affect the general appearance:
+screen_size = 1366, 768
+background_color = 255, 255, 255
+margin = 30
+full_screen = 1
+font_size = 43
+# These affect the behavior of the game:
+drop_speed = 2
+banana_speed = 7
+speed_increase = 0.5
+weights_per_level = 10
+banana_pad_top = 30
+banana_pad_side = 15
